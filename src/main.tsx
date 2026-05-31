4ceffcf275fc5fbaf9f9efde91196ecc8af98135
soroeru-afk <soroeru@gmail.com>
2026-06-01 08:43:19 +0900

    Set default PWA window size to 1440x900

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import { initSettings, applySettingsToDOM } from './settingsSync';
 
 initSettings();
 
+if (window.matchMedia('(display-mode: standalone)').matches) {
+  window.resizeTo(1440, 900);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
